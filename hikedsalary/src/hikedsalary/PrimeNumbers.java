82ecc7919f0603ad2975bfbb49c33db484efb08a
sghoradk <sharujerry@gmail.com>
2017-04-26 12:44:59 +0530

    testing

---
+++ hikedsalary/src/hikedsalary/PrimeNumbers.java
@@ -0,0 +1,32 @@
+package hikedsalary;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+	
+	public static void main(String[] args){
+
+			int i, num, flag=0;
+			Scanner reader = new Scanner(System.in);  // Reading from System.in
+			System.out.println("Enter a number: ");
+			num = reader.nextInt();
+			for(i=2; i<=num-1; i++)
+			{
+				if(num%i==0)
+				{
+					flag = 1;
+					break;
+				}
+			}
+			if(flag==1)
+			{
+				System.out.println(+num+ " is not a prime number");
+					
+			}
+			else
+			{
+				System.out.println(+num+ " is a prime number");
+			}
+				
+	}
+}
---
+++ hikedsalary/src/hikedsalary/StringConcatinate.java
@@ -0,0 +1,13 @@
+package hikedsalary;
+
+public class StringConcatinate {
+
+	public static void main(String[] args)
+	{
+		String string1 = "Hello";
+		String string2 = "World";
+		String concatinated = string1 + string2;
+		System.out.println(concatinated);
+	}
+}
+
